bc740ebffac4f476a1607ad577091ce595ffb08e
chase0213 <chase0213+dev@gmail.com>
2019-11-14 02:09:00 +0900

    update dependencies

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule ExAssets.MixProject do
     [
       {:ex_aws, "~> 2.1"},
       {:ex_aws_s3, "~> 2.0"},
+      {:poison, "~> 3.1"},
       {:hackney, "~> 1.9"},
-      {:sweet_xml, "~> 0.6"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
